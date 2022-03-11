60da179827eac4cfbb5baa8466a0c8ee455bdc83
Kallum Panditharatna <kallum179@gmail.com>
2022-03-11 04:37:13 +0000

    Interpolation search finished

--- Task A/InterpolationSearch/InterpolationSearch/InterpolationSearch.cpp
+++ Task A/InterpolationSearch/InterpolationSearch/InterpolationSearch.cpp
@@ -4,69 +4,142 @@
 
 
 
-std::vector<int> base;
+//std::vector<int> base;
+int* base;
 
-void FillListRandom(int amount) {
+//fills the array with a given amount of random numbers (sorted) between min and max
+int FillListRandom(int amount, int gapMin, int gapMax) {
 
+	base = new int[amount];
+	
+	int numberToAdd = 0;
+
+	//adds in the wanted amount of values
+	for (int i = 0; i < amount; i++) {
+
+		//gets the next random number
+		numberToAdd += rand() % (gapMax - gapMin) + gapMin;
+
+		//adds the random number
+		base[i] = numberToAdd;
+	}
+
+	return numberToAdd;
+}
+
+//fills the list with a given amount of sequential numbers
+void FillList(int amount, int step) {
+
+	base = new int[amount];
 	for (int i = 0; i < amount; i++)
-		base.push_back(rand() % 1000);
+		base[i] = i * step;
 }
 
-void FillList(int amount) {
-	for (int i = 0; i < amount; i++)
-		base.push_back(i);
-}
-
-void SortList() {
-
-	std::sort(base.begin(), base.end());
-}
-
+//outputs the full list
 void OutputList(std::vector<int> toOutput) {
 
 	for (int i = 0; i < toOutput.size(); i++)
 		std::cout << "Index: " << i << " Value: " << toOutput[i] << std::endl;
 }
 
-float InterpolateIndex(int key) {
+//outputs a slice of the vector
+void OutputList(int bottom, int top) {
+
+	for (int i = bottom; i <= top; i++)
+		std::cout << "Index: " << i << " Value: " << base[i] << std::endl;
+}
+
+//constrains a value between two bounds
+int Constrain(int value, int lowerBound, int upperBound) {
+	if (value < lowerBound)
+		return lowerBound;
+	else if (value > upperBound)
+		return upperBound;
+	return value;
+}
+
+//interpolation index search using passed start and end indexes
+int InterpolateIndex(int key, int bottom, int top) {
 
 	float k = key;
-	float top = base.size() - 1;
-	float bottom = 0;
 	float VTop = base[top];
 	float VBottom = base[bottom];
 
-	 return std::floor((top - bottom) * ((k - VBottom) / (VTop - VBottom)) + bottom);
+	return Constrain(std::floor((top - bottom) * ((k - VBottom) / (VTop - VBottom)) + bottom), bottom, top);
 }
 
-bool InterpolateSearch(int key, std::vector<int> V) {
+//interpolation search. Return the found index, or -1 if not found
+int InterpolateSearch(int key, int comparisonCount, int bottom, int top) {
 
-	std::cout << std::endl;
-	std::cout << std::endl;
+	//outputs the array
+	OutputList(bottom, top);
+	std::cout << "^^ Array to search ^^ " << std::endl;
 
-	std::cout << "Array to search: " << std::endl;
-	OutputList(V);
+	//outputs the array size
+	std::cout << "Array size left to check: " <<  1 + top - bottom << std::endl;
 
+	std::cout << "Start: " << bottom << ", End: " << top << std::endl;
+
+	//outputs what key is being searched for
 	std::cout << std::endl;
 	std::cout << "Searching for index of key: " << key << std::endl;
 
-	int estimatedPosition = InterpolateIndex(key);
-	int valueAtEstimate = V[estimatedPosition];
-	std::cout << "Estimated position: " << estimatedPosition << std::endl;
-	std::cout << "Value at estimated position " << V[estimatedPosition] << std::endl;
+	//checks if only one value is left to check
+	if (bottom == top)
 
-	return key == valueAtEstimate;
+		//return if the last value was the right one
+		if (base[bottom] == key)
+			return bottom;
+		else
+			return -1;
+
+	//gets the estimated position (using the interpolation algorithm)
+	int estimatedPosition = InterpolateIndex(key, bottom, top);
+	std::cout << "Estimated position: " << estimatedPosition << std::endl;
+
+	//gets the value at the estimated position
+	int valueAtEstimate = base[estimatedPosition];
+	std::cout << "Value at estimated position " << base[estimatedPosition] << std::endl;
+
+	//increases the number of comparisons done
+	comparisonCount++;
+	std::cout << "Comparisons done: " << comparisonCount << std::endl;
+
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+
+	if (valueAtEstimate == key)
+		return estimatedPosition;
+	else if (key < valueAtEstimate)
+		return InterpolateSearch(key, comparisonCount, bottom, Constrain(estimatedPosition - 1, bottom, top));
+	else if (key > valueAtEstimate)
+		return InterpolateSearch(key, comparisonCount, Constrain(estimatedPosition + 1, bottom, top), top);
 }
 
-int main()
-{
-
-	FillListRandom(200);
-	SortList();
-	OutputList(base);
-	std::cout << "^^^ Array filled out" << std::endl;
 
 
-	InterpolateSearch(40, base);
 
+int main() {
+
+	std::string input;
+
+	do {
+
+		int arrayCount = 10;
+		int gapMin = 10;
+		int gapMax= 0;
+
+		int maxVal = FillListRandom(arrayCount, gapMin, gapMax);
+
+		if (InterpolateSearch(rand() % maxVal, 0, 0, arrayCount - 1))
+			std::cout << "Found" << std::endl;
+		else
+			std::cout << "Not found" << std::endl;
+
+		std::cin >> input;
+		system("cls");
+
+	} while (input != "x");
 }
