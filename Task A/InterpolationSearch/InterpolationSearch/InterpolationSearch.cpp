1de852c57d3c4cfdadb2ce8c5af9a479558697e3
Kallum Panditharatna <kallum179@gmail.com>
2022-03-12 11:15:46 +0000

    Linear and exponential search implemented

--- Task A/InterpolationSearch/InterpolationSearch/InterpolationSearch.cpp
+++ Task A/InterpolationSearch/InterpolationSearch/InterpolationSearch.cpp
@@ -6,6 +6,7 @@
 
 //std::vector<int> base;
 int* base;
+int comparisonCount;
 
 //fills the array with a given amount of random numbers (sorted) between min and max
 int FillListRandom(int amount, int gapMin, int gapMax) {
@@ -66,7 +67,7 @@ int InterpolateIndex(int key, int bottom, int top) {
 }
 
 //interpolation search. Return the found index, or -1 if not found
-int InterpolateSearch(int key, int comparisonCount, int bottom, int top, bool showArray) {
+int InterpolateSearch(int key, int bottom, int top, bool showArray, bool showAnyCLI) {
 
 	if (showArray) {
 
@@ -76,13 +77,17 @@ int InterpolateSearch(int key, int comparisonCount, int bottom, int top, bool sh
 	}
 
 	//outputs the array size
-	std::cout << "Array size left to check: " << 1 + top - bottom << std::endl;
+	if (showAnyCLI) {
 
-	std::cout << "Start: " << bottom << ", End: " << top << std::endl;
+		std::cout << "Array size left to check: " << 1 + top - bottom << std::endl;
 
-	//outputs what key is being searched for
-	std::cout << std::endl;
-	std::cout << "Searching for index of key: " << key << std::endl;
+
+		std::cout << "Start: " << bottom << ", End: " << top << std::endl;
+
+		//outputs what key is being searched for
+		std::cout << std::endl;
+		std::cout << "Searching for index of key: " << key << std::endl;
+	}
 
 	//checks if only one value is left to check
 	if (bottom == top)
@@ -95,30 +100,106 @@ int InterpolateSearch(int key, int comparisonCount, int bottom, int top, bool sh
 
 	//gets the estimated position (using the interpolation algorithm)
 	int estimatedPosition = InterpolateIndex(key, bottom, top);
-	std::cout << "Estimated position: " << estimatedPosition << std::endl;
+	if (showAnyCLI) std::cout << "Estimated position: " << estimatedPosition << std::endl;
 
 	//gets the value at the estimated position
 	int valueAtEstimate = base[estimatedPosition];
-	std::cout << "Value at estimated position " << base[estimatedPosition] << std::endl;
+	if (showAnyCLI) std::cout << "Value at estimated position " << base[estimatedPosition] << std::endl;
 
 	//increases the number of comparisons done
 	comparisonCount++;
-	std::cout << "Comparisons done: " << comparisonCount << std::endl;
 
-
-	std::cout << std::endl;
-	std::cout << std::endl;
+	if (showAnyCLI) {
+		std::cout << "Comparisons done: " << comparisonCount << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+	}
 
 	//the recursive calls have constrains to stop the ends from overlapping
 	if (valueAtEstimate == key)
 		return estimatedPosition;
 	else if (key < valueAtEstimate)
-		return InterpolateSearch(key, comparisonCount, bottom, Constrain(estimatedPosition - 1, bottom, top), showArray);
+		return InterpolateSearch(key, bottom, Constrain(estimatedPosition - 1, bottom, top), showArray, showAnyCLI);
 	else if (key > valueAtEstimate)
-		return InterpolateSearch(key, comparisonCount, Constrain(estimatedPosition + 1, bottom, top), top, showArray);
+		return InterpolateSearch(key, Constrain(estimatedPosition + 1, bottom, top), top, showArray, showAnyCLI);
+}
+
+//returns the found index by looping sequentially, or returns -1 for not found
+int LinearSearch(int key, int bottom, int top) {
+
+	//loops through each value
+	for (int i = bottom; i <= top; i++) {
+
+		comparisonCount++;
+
+		//checks if this was the value
+		if (base[i] == key)
+
+			return  i;
+	}
+
+	return -1;
+}
+
+//Returns the middle value if the key was found there, if not recur with the half it could be in. Returns -1 if not found
+int BinarySearch(int key, int bottom, int top) {
+
+	//finds the middle
+	int middle = std::floor(bottom + (top - bottom) / 2);
+
+	comparisonCount++;
+	//if the key was found in the middle
+	if (base[middle] == key)
+		return middle;
+
+	//if there was no more values left to check
+	if (top == bottom)
+		return -1;
+
+	//recalls the search
+	if (key < base[middle])
+		BinarySearch(key, bottom, Constrain(middle - 1, bottom, middle));
+	else if (key > base[middle])
+		BinarySearch(key, Constrain(middle + 1, middle, top), top);
+}
+
+//finds the search space and then calls binary search on it
+int ExponentialSearch(int key, int bottom, int top) {
+
+	comparisonCount++;
+	//check if key was present at the first address
+	if (base[bottom] == key)
+		return bottom;
+
+	int i = bottom + 1;
+	//keeps looping while there is still array space to check and the current index is smaller than the key
+	while (i < top && base[i] <= key) {
+
+		comparisonCount++;
+
+		//doubles the seach space
+		i = i * 2;
+	}
+
+	//calls binary search on the found search space
+	return BinarySearch(key, i / 2, std::min(i, top));
 }
 
 
+void OutputFinalResult(int key, int returnedIndex) {
+
+	//outputs the result
+	if (returnedIndex != -1) {
+
+		std::cout << "Found at: " << returnedIndex << std::endl;
+		std::cout << "Key: " << key << std::endl;
+		std::cout << "Value at " << returnedIndex << ": " << base[returnedIndex] << std::endl;
+		std::cout << "Comparisons done: " << comparisonCount << std::endl;
+
+	} else
+		std::cout << "Not found" << std::endl;
+
+}
 
 
 int main() {
@@ -127,10 +208,11 @@ int main() {
 
 	do {
 
+		//filling the array
+
 		int arrayCount = 10000000;
 		int gapMin = 10;
 		int gapMax = 0;
-
 
 		std::cout << "Filling array with random numbers" << std::endl;
 
@@ -140,30 +222,46 @@ int main() {
 		//fills with sequential data with a constant gap
 		//int maxVal = FillList(arrayCount, 4);
 
-		std::cout << "Array filled" << std::endl;
+		std::cout << "Array filled, generating random key to search for" << std::endl;
 
 
-		//gets a random key
+		//gets a random key (might not exist)
 		//int key = rand() % maxVal;
 
 		//gets a key that exists
 		int key = base[rand() % arrayCount];
 
+		std::cout << "Key generated " << std::endl;
 		std::cout << std::endl;
 
 
+		//searches
+
+		int returnedIndex;
+		comparisonCount = 0;
+
 		//calls the interpolation search
-		int returnedIndex = InterpolateSearch(key, 0, 0, arrayCount - 1, false);
+		std::cout << "vv Interpolation search vv" << std::endl;
+		returnedIndex = InterpolateSearch(key, 0, arrayCount - 1, false, false);
+		OutputFinalResult(key, returnedIndex);
 
-		//outputs the result
-		if (returnedIndex != -1) {
 
-			std::cout << "Found at: " << returnedIndex << std::endl;
-			std::cout << "Key: " << key << std::endl;
-			std::cout << "Value at " << returnedIndex << ": " << base[returnedIndex] << std::endl;
+		std::cout << std::endl;
+		comparisonCount = 0;
 
-		} else
-			std::cout << "Not found" << std::endl;
+		//calls the linear search
+		std::cout << "vv Linear search vv" << std::endl;
+		returnedIndex = LinearSearch(key, 0, arrayCount - 1);
+		OutputFinalResult(key, returnedIndex);
+
+
+		std::cout << std::endl;
+		comparisonCount = 0;
+
+		//calls the linear search
+		std::cout << "vv Exponential search vv" << std::endl;
+		returnedIndex = ExponentialSearch(key, 0, arrayCount - 1);
+		OutputFinalResult(key, returnedIndex);
 
 
 		std::cin >> input;
